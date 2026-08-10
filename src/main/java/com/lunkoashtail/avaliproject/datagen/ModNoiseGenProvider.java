a7e0fb0e07dd3889893dfaae54a3c62d0ac4f3bf
989onan <onanchew@gmail.com>
2026-08-10 16:44:41 -0400

    Add Avalon dimension (Kinda works)
    add a dimension that compiles these biomes via a random noise gen that is really bad, then make the water Ammonia and slap a random orbit dimension for ad astra if it exists...

---
+++ src/main/java/com/lunkoashtail/avaliproject/datagen/ModNoiseGenProvider.java
@@ -0,0 +1,58 @@
+package com.lunkoashtail.avaliproject.datagen;
+
+import com.google.common.collect.ImmutableList;
+import com.lunkoashtail.avaliproject.AvaliProject;
+import com.lunkoashtail.avaliproject.block.ModBlocks;
+import com.lunkoashtail.avaliproject.block.fluid.Ammonia;
+import com.lunkoashtail.avaliproject.datagen.avalon.AvalonBiomes;
+import com.lunkoashtail.avaliproject.datagen.avalon.AvalonTags;
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.HolderGetter;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.data.worldgen.SurfaceRuleData;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.KeyDispatchDataCodec;
+import net.minecraft.world.level.biome.Biomes;
+import net.minecraft.world.level.biome.OverworldBiomeBuilder;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.dimension.DimensionType;
+import net.minecraft.world.level.dimension.LevelStem;
+import net.minecraft.world.level.levelgen.*;
+import net.minecraft.world.level.levelgen.feature.stateproviders.NoiseBasedStateProvider;
+import net.minecraft.world.level.levelgen.synth.NormalNoise;
+import net.minecraft.world.level.material.WaterFluid;
+import net.neoforged.neoforge.common.Tags;
+
+import java.util.stream.Stream;
+
+public class ModNoiseGenProvider extends NoiseRouterData {
+    public static final ResourceKey<NoiseGeneratorSettings> AVALON_NOISE = ResourceKey.create(Registries.NOISE_SETTINGS,
+            ResourceLocation.fromNamespaceAndPath(AvaliProject.MOD_ID, "excessive_oceans"));
+
+
+
+    public static void bootstrapType(BootstrapContext<NoiseGeneratorSettings> context) {
+        HolderGetter<NormalNoise.NoiseParameters> pNoiseParameters = context.lookup(Registries.NOISE);
+        HolderGetter<DensityFunction> DensityRegistry = context.lookup(Registries.DENSITY_FUNCTION);
+        context.register(NoiseGeneratorSettings.OVERWORLD, new NoiseGeneratorSettings(
+                new NoiseSettings(-64, 384, 1, 1),
+                Blocks.STONE.defaultBlockState(),
+                ModBlocks.AMMONIA_BLOCK.get().defaultBlockState(),
+                NoiseRouterData.overworld(DensityRegistry, pNoiseParameters,true,false),
+                SurfaceRuleData.overworld(),
+                (new OverworldBiomeBuilder()).spawnTarget(),
+                63,
+                false,
+                true,
+                false,
+                false
+        ));
+
+
+
+    }
+
+}
